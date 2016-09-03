737ad54e63d80a2bd13da54e095c546722d7521f
Kevin McKernan <kevin@mckernan.in>
2016-09-03 12:01:24 -0600

    remove unused variable

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Form from './Form';
 import '../assets/App.css';
-let isEmbed = window != window.parent;
 
 class App extends Component {
   render() {
