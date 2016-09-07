c50831c42b6ac417e563de9b4f70c3bfc0fb5b6a
Kevin McKernan <kevin@mckernan.in>
2016-09-06 21:28:39 -0600

    move analytics to a node module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './components/App';
 import './assets/index.css';
 
+ReactGA.initialize('UA-52435052-3');
+
+function logPageView() {
+	ReactGA.pageview(window.location.hash);
+}
+
 ReactDOM.render(
-  <App />,
+  <App onload={logPageView}/>,
   document.getElementById('root')
 );
